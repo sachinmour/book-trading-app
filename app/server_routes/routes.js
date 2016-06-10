faee2e94cea5308df370f34717c6b5348a3dbd29
Sachin Mour <rtr.sachinmour@gmail.com>
2016-06-10 20:19:24 +0000

    Added server rendering

--- app/server_routes/routes.js
+++ app/server_routes/routes.js
@@ -6,9 +6,9 @@ module.exports = function(app, passport) {
     // =====================================
     // HOME PAGE (with login links) ========
     // =====================================
-    // app.get('/', function(req, res) {
-    //     serverRender.handleRender(req, res);
-    // });
+    app.get('/', function(req, res) {
+        serverRender.handleRender(req, res);
+    });
     
     // app.post('/addStock', function(req, res) {
     //     stockHandler.addStock(req, res);
