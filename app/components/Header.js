35bc210f4116d1405a5c66ffb35013be5a7d216b
Sachin Mour <rtr.sachinmour@gmail.com>
2016-06-12 22:24:29 +0100

    Edited Header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 
 class Header extends React.Component{
   
+  constructor(props, context){
+    super(props, context);
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentWillMount() {
+    var _this = this;
+    axios.get('/getUser')
+    .then(function (response) {
+      if (!response.data.redirect) {
+        _this.setState({user: response.data});
+      }
+    });
+  }
+
   render() {
+    
+    var links;
+
+    if (this.state.user.displayName) {
+      links =  <div><a href="/all">All Books</a>
+                <a href="/my">My Books</a>
+                <a href="/setting"><i class="fa fa-cog" aria-hidden="true"></i></a>
+                <a href="/logout"><i class="fa fa-power-off" aria-hidden="true"></i></a></div>;
+    } else {
+      links = <a href="/auth/twitter">Login</a>;
+    }
+
     return (
       <div id="top">
           <div id="header">
-              <div id="name"><a href="/">booktrade</a></div>
+              <div id="name">
+                <a href="/">booktrade</a>
+              </div>
               <div id="authenticated">
-                  <a href="/">Login</a>
-                  <a href="/">Login</a>
-                  <a href="/">Login</a>
-                  <a href="/">Login</a>
+                  {links}
               </div>
           </div>
       </div>
